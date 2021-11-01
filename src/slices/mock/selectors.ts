e55c8c0a74d156d3de0882b0e867d7a375435130
krusalovorg <krusalovpro@gmail.com>
2021-11-01 17:21:51 +0400

    fix error build

--- src/slices/mock/selectors.ts
+++ src/slices/mock/selectors.ts
@@ -1,7 +1,7 @@
 import { State, moduleName } from '@src/slices/mock';
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectMock = createSelector<FixMeAny, State, FixMeAny>(
+export const selectMock = createSelector<FixMeAny[], State>(
   (state) => state[moduleName],
   (data) => data,
 );
--- src/slices/themeSlice/selectors.ts
+++ src/slices/themeSlice/selectors.ts
@@ -1,7 +1,7 @@
 import { State, moduleName } from '@src/slices/themeSlice';
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectTheme = createSelector<FixMeAny, State, FixMeAny>(
+export const selectTheme = createSelector<FixMeAny[], State>(
   (state) => state[moduleName],
   (data) => data,
 );
